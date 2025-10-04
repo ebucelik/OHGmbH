0ac166d2ed0ffb346673127be3036a78cce235a3
Ing. Ebu Celik <ebucelik1@hotmail.com>
2025-10-04 11:53:48 +0200

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,38 +8,38 @@ import React from "react";
 
 export default function Header() {
     enum Menu {
-        solution = "Solution",
-        service = "Service",
+        solution = "Lösungen",
+        service = "Leistungen",
         team = "Team",
-        contact = "Contact",
+        contact = "Kontakt",
         default = ""
     }
     const [showNavBar, setShowNavBar] = useState(false);
-    const [hideHeaderWhileScrollingDown, setHideHeaderWhileScrollingDown] = useState("translate-y-0");
-    const [showMenu, setShowMenu] = useState(Menu.default);
+    const [hideHeaderWhileScrollingDown, setHideHeaderWhileScrollingDown] = useState("sm:translate-y-0");
+    const [menu, setMenu] = useState(Menu.default);
 
     var previousScrollY = 0
 
     function toggleShowNavBar() {
-        setShowMenu(Menu.default);
+        setMenu(Menu.default);
         setShowNavBar(!showNavBar);
     }
 
     React.useEffect(() => {
         window.addEventListener("wheel", (event) => {
             if (event.deltaY > 0) {
-                setHideHeaderWhileScrollingDown("-translate-y-100");
+                setHideHeaderWhileScrollingDown("sm:-translate-y-100");
             } else {
-                setHideHeaderWhileScrollingDown("translate-y-0");
+                setHideHeaderWhileScrollingDown("sm:translate-y-0");
             }
         })
 
         window.addEventListener("scroll", () => {
             if (innerWidth < 768) {
                 if (previousScrollY > scrollY || scrollY <= 0) {
-                    setHideHeaderWhileScrollingDown("translate-y-0");
+                    setHideHeaderWhileScrollingDown("sm:translate-y-0");
                 } else {
-                    setHideHeaderWhileScrollingDown("-translate-y-100");
+                    setHideHeaderWhileScrollingDown("sm:-translate-y-100");
                 }
             }
 
@@ -47,14 +47,33 @@ export default function Header() {
         })
     }, []);
 
-    function menu(): ReactElement {
-        switch (showMenu) {
+    function menuBody(): ReactElement {
+        return <div
+            className={`flex flex-col sm:flex-row sm:block ${menu == Menu.default ? 'hidden' : 'pt-5'}`}
+            onMouseEnter={() => setMenu(menu)}
+            onMouseLeave={() => setMenu(Menu.default)}
+        >
+            <div className="flex flex-row sm:hidden" onClick={() => setMenu(Menu.default)}>
+                <div>
+                    <Icon icon="material-symbols:chevron-left-rounded" className="h-8 w-8" />
+                </div>
+                <div className="text-xs place-content-center">
+                    {
+                        menu
+                    }
+                </div>
+            </div>
+
+            {
+                menuBodies()
+            }
+        </div>;
+    }
+
+    function menuBodies(): ReactElement {
+        switch (menu) {
             case Menu.solution:
-                return <div
-                    className="flex flex-col sm:flex-row fade-down-05s py-5 gap-2 sm:gap-10"
-                    onMouseEnter={() => setShowMenu(Menu.solution)}
-                    onMouseLeave={() => setShowMenu(Menu.default)}
-                >
+                return <div className="flex flex-col sm:flex-row fade-in-05s gap-1 sm:gap-10">
                     <div className="sm:flex-1"></div>
                     <div className="flex-2">
                         <div className="font-bold sm:text-lg">
@@ -234,11 +253,11 @@ export default function Header() {
 
             default:
                 return <div></div>;
-        }
+        };
     }
 
     return (
-        <nav className={`${hideHeaderWhileScrollingDown} sticky top-0 z-10 mb-5 transition duration-300 opacity-95`}>
+        <nav className={`${hideHeaderWhileScrollingDown} sticky top-0 z-10 mb-5 easeIn transition duration-300 opacity-95`}>
             <div className="w-full p-5 rounded-b-2xl bg-white border-b-1 border-gray-200 shadow-lg fade-down-1s">
                 <div className="flex flex-row">
                     <div className="flex-2 font-bold">
@@ -289,30 +308,30 @@ export default function Header() {
                     </div>
                     <div className="hidden mx-auto w-2/4 sm:flex flex-row text-xl font-bold text-center">
                         <div
-                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${showMenu == Menu.solution ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
-                            onMouseEnter={() => setShowMenu(Menu.solution)}
-                            onMouseLeave={() => setShowMenu(Menu.default)}
+                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${menu == Menu.solution ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
+                            onMouseEnter={() => setMenu(Menu.solution)}
+                            onMouseLeave={() => setMenu(Menu.default)}
                         >
                             Lösungen
                         </div>
                         <div
-                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${showMenu == Menu.service ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
-                            onMouseEnter={() => setShowMenu(Menu.service)}
-                            onMouseLeave={() => setShowMenu(Menu.default)}
+                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${menu == Menu.service ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
+                            onMouseEnter={() => setMenu(Menu.service)}
+                            onMouseLeave={() => setMenu(Menu.default)}
                         >
                             Leistungen
                         </div>
                         <div
-                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${showMenu == Menu.team ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
-                            onMouseEnter={() => setShowMenu(Menu.team)}
-                            onMouseLeave={() => setShowMenu(Menu.default)}
+                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${menu == Menu.team ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
+                            onMouseEnter={() => setMenu(Menu.team)}
+                            onMouseLeave={() => setMenu(Menu.default)}
                         >
                             Team
                         </div>
                         <div
-                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${showMenu == Menu.contact ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
-                            onMouseEnter={() => setShowMenu(Menu.contact)}
-                            onMouseLeave={() => setShowMenu(Menu.default)}
+                            className={`flex-1 decoration-appPrimary content-center rounded-lg duration-500 cursor-pointer ${menu == Menu.contact ? 'decoration-3 underline underline-offset-7 scale-104' : ''}`}
+                            onMouseEnter={() => setMenu(Menu.contact)}
+                            onMouseLeave={() => setMenu(Menu.default)}
                         >
                             Kontakt
                         </div>
@@ -323,38 +342,38 @@ export default function Header() {
                 </div>
                 <div className="hidden sm:block">
                     {
-                        menu()
+                        menuBody()
                     }
                 </div>
-                <div className={`fade-in-05s flex flex-col gap-3 pt-5 sm:hidden text-lg font-bold text-left ${showNavBar ? 'block' : 'hidden'}`}>
+                <div className={`fade-in-05s flex flex-col gap-3 pt-5 sm:hidden text-lg font-bold text-left ${showNavBar ? 'block' : 'hidden'} ${menu != Menu.default ? 'hidden' : ''}`}>
                     <div
-                        className={`decoration-appPrimary content-center rounded-lg ${showMenu == Menu.solution ? 'decoration-3 underline underline-offset-7' : ''}`}
-                        onClick={() => setShowMenu(showMenu != Menu.solution ? Menu.solution : Menu.default)}
+                        className={`decoration-appPrimary content-center rounded-lg ${menu == Menu.solution ? 'decoration-3 underline underline-offset-7' : ''}`}
+                        onClick={() => setMenu(menu != Menu.solution ? Menu.solution : Menu.default)}
                     >
                         <Link href="">Lösungen</Link>
                     </div>
                     <div
-                        className={`decoration-appPrimary content-center rounded-lg ${showMenu == Menu.service ? 'decoration-3 underline underline-offset-7' : ''}`}
-                        onClick={() => setShowMenu(showMenu != Menu.service ? Menu.service : Menu.default)}
+                        className={`decoration-appPrimary content-center rounded-lg ${menu == Menu.service ? 'decoration-3 underline underline-offset-7' : ''}`}
+                        onClick={() => setMenu(menu != Menu.service ? Menu.service : Menu.default)}
                     >
                         <Link href="">Leistungen</Link>
                     </div>
                     <div
-                        className={`decoration-appPrimary content-center rounded-lg ${showMenu == Menu.team ? 'decoration-3 underline underline-offset-7' : ''}`}
-                        onClick={() => setShowMenu(showMenu != Menu.team ? Menu.team : Menu.default)}
+                        className={`decoration-appPrimary content-center rounded-lg ${menu == Menu.team ? 'decoration-3 underline underline-offset-7' : ''}`}
+                        onClick={() => setMenu(menu != Menu.team ? Menu.team : Menu.default)}
                     >
                         <Link href="">Team</Link>
                     </div>
                     <div
-                        className={`decoration-appPrimary content-center rounded-lg ${showMenu == Menu.contact ? 'decoration-3 underline underline-offset-7' : ''}`}
-                        onClick={() => setShowMenu(showMenu != Menu.contact ? Menu.contact : Menu.default)}
+                        className={`decoration-appPrimary content-center rounded-lg ${menu == Menu.contact ? 'decoration-3 underline underline-offset-7' : ''}`}
+                        onClick={() => setMenu(menu != Menu.contact ? Menu.contact : Menu.default)}
                     >
                         <Link href="">Kontakt</Link>
                     </div>
                 </div>
                 <div className="sm:hidden">
                     {
-                        menu()
+                        menuBody()
                     }
                 </div>
             </div>
